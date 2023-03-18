2e3e3705574cb4175193101da63096b7cd828fc7
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-18 20:23:46 +0600

    Api CBV Created

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-    
+    path('devices/',views.DeviceList.as_view(),name='device_list'),
+    path('devices/<int:pk>/',views.DeviceDetail.as_view(),name='device_detail'),
+    path('device_log/',views.DeviceLogList.as_view(),name='device_log'),
+    path('device_log/<int:pk>/',views.DeviceLogDetail.as_view(),name='device_log_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Device,DeviceLog
+from rest_framework import generics
+from .serializers import DeviceSerializer,DeviceLogSerializer
 
 # Create your views here.
+
+""" DeviceList provides the list
+and create operations"""
+
+class DeviceList(generics.ListCreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+
+"""
+DeviceDetail provides the retrieve, 
+update, and destroy operations.
+"""
+
+class DeviceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+
+class DeviceLogList(generics.ListCreateAPIView):
+    queryset = DeviceLog.objects.all()
+    serializer_class = DeviceLogSerializer
+
+class DeviceLogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DeviceLog.objects.all()
+    serializer_class = DeviceLogSerializer
+
