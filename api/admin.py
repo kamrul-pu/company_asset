96c116bfadd35cc4ec7c1fc2bf0e0132092ff344
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-18 20:00:16 +0600

    Models Added in the admin Panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Company,Employee,Device,DeviceLog
 # Register your models here.
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['name','condition','company']
+
+class DeviceLogAdmin(admin.ModelAdmin):
+    list_display = ['device','employee','checked_out','checked_in','condition']
+
+admin.site.register(Company)
+admin.site.register(Employee)
+admin.site.register(Device,DeviceAdmin)
+admin.site.register(DeviceLog,DeviceLogAdmin)
