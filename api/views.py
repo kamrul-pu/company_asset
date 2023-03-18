12b0ca90b0b8c5baf391a6c061e0c89e535b5564
Kamrul Hasan <kamrul.h456@gmail.com>
2023-03-18 21:51:29 +0600

    Authentication Added

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Device,DeviceLog
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .serializers import DeviceSerializer,DeviceLogSerializer
 
 # Create your views here.
@@ -9,8 +9,15 @@ from .serializers import DeviceSerializer,DeviceLogSerializer
 and create operations"""
 
 class DeviceList(generics.ListCreateAPIView):
-    queryset = Device.objects.all()
+    # queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        company = self.request.user.employee.company
+        print('user=',self.request.user)
+        queryset = Device.objects.filter(company=company)
+        return queryset
 
 
 """
@@ -19,15 +26,44 @@ update, and destroy operations.
 """
 
 class DeviceDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Device.objects.all()
+    # queryset = Device.objects.all()
     serializer_class = DeviceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        company = self.request.user.employee.company
+        queryset = Device.objects.filter(company=company)
+        obj = get_object_or_404(queryset, pk=self.kwargs['pk'])
+        return obj
 
 
+""" DeviceLogList provides the list
+and create operations"""
 class DeviceLogList(generics.ListCreateAPIView):
-    queryset = DeviceLog.objects.all()
+    # queryset = DeviceLog.objects.all()
     serializer_class = DeviceLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        company = self.request.user.employee.company
+        queryset = DeviceLog.objects.filter(device__company=company)
+        return queryset
+
+
+"""
+DeviceLogDetail provides the retrieve, 
+update, and destroy operations.
+"""
 class DeviceLogDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = DeviceLog.objects.all()
+    # queryset = DeviceLog.objects.all()
     serializer_class = DeviceLogSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        company = self.request.user.employee.company
+        queryset = DeviceLog.objects.filter(device__company=company)
+        obj = get_object_or_404(queryset,pk=self.kwargs['pk'])
+
+
+
 
